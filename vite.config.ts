43e8e4863ce7d9194e8eb02be93d01544c9fe2dc
aamistan <aaron.stanley@fwcs.k12.in.us>
2025-04-30 19:49:29 +0000

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,7 @@ export default defineConfig(() => {
     host: '0.0.0.0',
     port: 5173,
     strictPort: true,
+    https: false,
     cors: true,
     headers: {
       'Access-Control-Allow-Origin': '*',
