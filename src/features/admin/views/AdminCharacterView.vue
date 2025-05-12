9b3959cd841f22849950a135ae134c9792917e77
aamistan <aaron.stanley@fwcs.k12.in.us>
2025-05-12 11:46:27 +0000

    chore: reorder imports

--- src/features/admin/views/AdminCharacterView.vue
+++ src/features/admin/views/AdminCharacterView.vue
@@ -13,11 +13,10 @@
 
 <script setup lang="ts">
 // Admin dashboard widgets for managing character classes & species
-import DashboardWidgetGrid from '@/features/admin/components/dashboard/DashboardWidgetGrid.vue'
-
-import CharacterTableWidget from '@/features/admin/components/characterDashboard/CharacterTableWidget.vue'
 import CharacterMetricsWidget from '@/features/admin/components/characterDashboard/CharacterMetricsWidget.vue'
+import CharacterTableWidget from '@/features/admin/components/characterDashboard/CharacterTableWidget.vue'
 import FeaturedTagsWidget from '@/features/admin/components/characterDashboard/FeaturedTagsWidget.vue'
+import DashboardWidgetGrid from '@/features/admin/components/dashboard/DashboardWidgetGrid.vue'
 </script>
 
 <route>
