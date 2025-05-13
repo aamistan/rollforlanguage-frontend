0581541c1e5170109d6ff1ea8c3c9997e4f30c4e
aamistan <aaron.stanley@fwcs.k12.in.us>
2025-05-13 02:55:31 +0000

    rollback sidebar and widget (users dashboard) color changes

--- src/features/admin/components/userDashboard/UserDashboardSidebarTools.vue
+++ src/features/admin/components/userDashboard/UserDashboardSidebarTools.vue
@@ -3,7 +3,7 @@
     <button
       v-for="tool in filteredTools"
       :key="tool.name"
-      class="flex items-center gap-2 px-4 py-2 bg-rose-50 text-rose-900 hover:bg-rose-100 dark:bg-rose-900 dark:text-rose-100 dark:hover:bg-rose-800 rounded text-left"
+      class="flex items-center gap-2 px-4 py-2 bg-white text-black hover:bg-gray-400 rounded text-left"
       @click="handleToolClick(tool.action!)"
     >
       <AppIcon :name="tool.icon" :library="tool.library" />
